7777cf59550d33ce5ec5d83fde9863eb570bc5e0
Leon Davis <srcfmt@pm.me>
2021-06-29 04:37:53 +0100

    feat: add cache control to post response

--- deploy.ts
+++ deploy.ts
@@ -1,4 +1,4 @@
-import { serve } from "https://deno.land/x/sift@0.3.2/mod.ts";
+import { serve, json } from "https://deno.land/x/sift@0.3.2/mod.ts";
 import { parse, Config } from "./deps.ts";
 
 const github = "https://github.com/ResponsiveDev/blog/raw/master";
@@ -32,25 +32,32 @@ serve({
     );
   },
   "/post/:slug": async (req, params): Promise<Response> => {
-    if(req.method.toLowerCase() !== "get") {
-      return new Response(
-        null,
-        {
-          status: 400,
-          headers: {
-            "Access-Control-Allow-Origin": "*"
-          }
-        }
+    if(req.method.toLowerCase() === "get") {
+      const post = await fetch(
+        `${config.github}/${config.out}/${params.slug}.json`,
+        { headers: { "Content-Type": "application/json" } }
       );
+
+      if(post.ok) {
+        return json(
+          post,
+          {
+            headers: {
+              "Access-Control-Allow-Origin": "*",
+              "Content-Type": "application/json",
+              "Cache-Control": "max-age=86400"
+            }
+          }
+        );
+      }
     }
-    return await fetch(
-      `${config.github}/${config.out}/${params.slug}.json`,
+
+    return new Response(
+      null,
       {
-        headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Content-Type": "application/json"
-        }
+        status: 400,
+        headers: { "Access-Control-Allow-Origin": "*" }
       }
     );
-  },
+  }
 });
